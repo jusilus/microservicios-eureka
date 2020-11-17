5669ec742696a531930231008efaec547471b418
David Domínguez López <daviddominguezlopez@gmail.com>
2020-11-17 19:40:18 +0100

    Prueba con comentario

--- src/main/java/com/formacionbdi/microservicios/app/eureka/MicroserviciosEurekaApplication.java
+++ src/main/java/com/formacionbdi/microservicios/app/eureka/MicroserviciosEurekaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class MicroserviciosEurekaApplication {
-
+	//Prueba para Git
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviciosEurekaApplication.class, args);
 	}
